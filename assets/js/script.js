99330abe6d02ebb47892699edaf870c84de1f2df
Antho <maxister@gmail.com>
2024-02-24 18:32:54 +0100

    Ajout logo en svg

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+// Ecouter les scroll (touch) vers le haut
+
+// Au moindre mouvement vers le haut, alors affiche la section suivante.
+
+// Pareil au moindre mouvement vers le bas, affiche la section précedente.
