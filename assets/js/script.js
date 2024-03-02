c894b38fdc819252fc2d194b3d365e08120d5758
Antho <maxister@gmail.com>
2024-03-02 12:34:48 +0100

    html/css/js integration

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,15 @@
 // Ecouter les scroll (touch) vers le haut
+window.addEventListener('scroll', function() {
+    const scrollPosition = window.scrollY;
+    const vh = window.innerHeight;
+    const threshold = 0.05; // Marge de tolérance pour prendre en compte les petites différences
+
+    if (scrollPosition <= vh * threshold) {
+        console.log("L'utilisateur est tout en haut du site.");
+    } else {
+        console.log("L'utilisateur n'est pas tout en haut du site.");
+    }
+});
 
 // Au moindre mouvement vers le haut, alors affiche la section suivante.
 
