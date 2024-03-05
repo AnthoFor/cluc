8a210cb563157da78c047307fb1c9aa9b63544c5
Antho <maxister@gmail.com>
2024-03-05 10:17:09 +0100

    Ajout du SCSS et des valeurs de position

---
+++ assets/js/functions.js
@@ -0,0 +1,64 @@
+// Functions
+function isScrollUp(currentOffset, lastKnownOffset) {
+    return currentOffset < lastKnownOffset;
+}
+
+export function goesToSection() {
+    if (window.scrollY == 0) {
+        console.log("L'utilisateur est tout en haut du site.");
+        // Remet le logo d'origine, supprime la navbar
+    } else {
+        // le logo se transforme dans la nav bar
+        // ---
+        // Au moindre mouvement vers le haut, alors affiche la section suivante.
+        if (isScrollUp(window.scrollY, lastKnownOffset)) {
+            if (window.scrollY < (sectionHeight * 4) && window.scrollY > (sectionHeight * 3)) {
+                console.log("en haut vers section 4");
+            } else if (window.scrollY < (sectionHeight * 3) && window.scrollY > (sectionHeight * 2)) {
+                console.log("en haut vers section 3")
+            } else if (window.scrollY < (sectionHeight * 2) && window.scrollY > (sectionHeight)) {
+                console.log("en haut vers section 2")
+            } else if (window.scrollY < sectionHeight) {
+                console.log("en haut vers section 1")
+            }
+        // Pareil au moindre mouvement vers le bas, affiche la section précedente.
+        } else {
+            if (window.scrollY > 0 && window.scrollY <= totalVh / totalSection) {
+                console.log("en bas vers section 3");
+                // window.scroll({
+                //     top: sectionHeight,
+                //     behavior: 'auto',
+                //     duration: 10
+                // });
+            } else if (window.scrollY > sectionHeight && window.scrollY < sectionHeight * 2) {
+                console.log("en bas vers section 3");
+            } else if (window.scrollY > (sectionHeight * 2) && window.scrollY < sectionHeight * 3) {
+                console.log("en bas vers section 4");
+            } else if (window.scrollY > (sectionHeight * 3) && window.scrollY < sectionHeight * 4) {
+                console.log("en bas vers section 5");
+            }
+        }
+    }
+    lastKnownOffset = window.scrollY;
+}
+
+export function toggleScrollListener(enableScroll) {
+    if (enableScroll) {
+        // Activer l'écoute du défilement
+        window.addEventListener('scroll', handleScroll);
+    } else {
+        // Désactiver l'écoute du défilement
+        window.removeEventListener('scroll', handleScroll);
+    }
+}
+
+function handleScroll(event) {
+    event.preventDefault();
+}
+
+// constante
+let lastKnownOffset = window.scrollY;
+const vh = window.innerHeight;
+const totalSection = document.querySelectorAll('.nobsContainer').length
+const totalVh = vh * totalSection;
+const sectionHeight = totalVh / totalSection;
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,6 @@
+import { goesToSection, toggleScrollListener } from "./functions.js";
+
 // Ecouter les scroll (touch) vers le haut
-window.addEventListener('scroll', function() {
-    const scrollPosition = window.scrollY;
-    const vh = window.innerHeight;
-    const threshold = 0.05; // Marge de tolérance pour prendre en compte les petites différences
-
-    if (scrollPosition <= vh * threshold) {
-        console.log("L'utilisateur est tout en haut du site.");
-    } else {
-        console.log("L'utilisateur n'est pas tout en haut du site.");
-    }
-});
-
-// Au moindre mouvement vers le haut, alors affiche la section suivante.
-
-// Pareil au moindre mouvement vers le bas, affiche la section précedente.
+// window.addEventListener('scroll', function(e) {
+//     goesToSection();
+// });
