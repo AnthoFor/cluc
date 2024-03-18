3e94b22f0481baff9f5f9636665a16cd3589823e
AnthoFor <maxister@gmail.com>
2024-03-18 12:13:33 +0100

    Debut de l'animation du fil + aiguille

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,9 +13,34 @@ window.addEventListener('resize', function(e) {
 })
 
 window.addEventListener('click', function(e){
-    if (e.target.id == 'needleNav') {
-        e.target.classList.toggle('needleToVertical')
+    if (e.target.id == 'needleNav' || e.target.id == "svgCalque") {
+        svgCalque.style.display = "initial";
+        if (needleNav.classList.contains('needleToVertical')) {
+            needleNav.classList.remove('needleToVertical')
+            needleNav.classList.add('needleToHorizontal');
+            monChemin.animate({
+                "stroke-dashoffset": longueurChemin
+            }, 400);
+        } else {
+            needleNav.classList.add('needleToVertical')
+            needleNav.classList.remove('needleToHorizontal');
+            monChemin.animate({
+                "stroke-dashoffset": 1
+            }, 400);
+        }
     }
 })
 
 AOS.init();
+
+var svg = Snap("#svgCalque");
+var monChemin = svg.select("#path");
+
+// Longueur totale du chemin du "path"
+var longueurChemin = Snap.path.getTotalLength(monChemin);
+
+// Définir la longueur du chemin à 0 au début de l'animation
+monChemin.attr({
+    "stroke-dasharray": longueurChemin + " " + longueurChemin,
+    "stroke-dashoffset": longueurChemin
+});
