f0e5d1a4a26ea8f029b37b46a6a4735f3b5d6007
AnthoFor <maxister@gmail.com>
2024-04-12 11:11:09 +0200

    test du menu sur iphone? Verra bien

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,3 +1,5 @@
+gsap.registerPlugin(MotionPathPlugin);
+
 // Functions
 function isScrollUp(currentOffset, lastKnownOffset) {
     return currentOffset < lastKnownOffset;
@@ -63,6 +65,64 @@ const distanceRight = window.innerWidth - rect.right;
 kikooDiv.style.right = (distanceRight / 10) + 'px';
 }
 
+export function menuAnimation(collapse) {
+    console.log(collapse);
+    if (collapse) {
+        needleNav.classList.remove('needleToVertical')
+        needleNav.classList.add('needleToHorizontal');
+        monChemin.animate({
+            "stroke-dashoffset": longueurChemin
+        }, 500);
+        gsap.to("#rect", {
+            motionPath: {
+                path: "#path",
+                align: "#path",
+                autoRotate: true,
+                alignOrigin: [0, 0],
+                yoyo:true,
+                curviness:2,
+                start: 1,
+                end: 0
+            },
+            transformOrigin: "80% 0%",
+            duration: 0.5,
+        });
+        countMenu = true;
+        setTimeout(function() {
+            svgCalque.style.opacity = "0";
+        }, 500);
+        needleCross.classList.remove('opacityPlus');
+        needleCross.classList.add('opacityLess');
+    } else {
+        // Pas compris le décalage, mais besoin de décaler à partir du 2e trigger
+        let translateX = countMenu ? 2 : 0;
+        svgCalque.style.display = "initial";
+        needleNav.classList.add('needleToVertical')
+        needleNav.classList.remove('needleToHorizontal');
+        monChemin.animate({
+            "stroke-dashoffset": 1
+        }, 500);
+        gsap.to("#rect", {
+            motionPath: {
+                path: "#path",
+                align: "#path",
+                autoRotate: true,
+                alignOrigin: [translateX, 0],
+                yoyo:true,
+                curviness:2,
+            },
+            transformOrigin: "80% 0%",
+            duration: 0.5,
+        });
+        svgCalque.style.opacity = "100"
+        needleCross.classList.add('opacityPlus');
+        needleCross.classList.remove('opacityLess');
+    }
+}
+
+
+
+
 // Variables/Constantes
 let lastKnownOffset = window.scrollY;
 let TxtAtelierClucWidth = document.getElementById('txtAtelierCluc').offsetWidth;
@@ -71,3 +131,15 @@ let vh = window.innerHeight;
 let totalSection = document.querySelectorAll('.nobsContainer').length
 let totalVh = vh * totalSection;
 let sectionHeight = totalVh / totalSection;
+let countMenu = false;
+var svg = Snap("#svgCalque");
+var monChemin = svg.select("#path");
+
+// Longueur totale du chemin du "path"
+var longueurChemin = Snap.path.getTotalLength(monChemin);
+
+// Définir la longueur du chemin à 0 au début de l'animation
+monChemin.attr({
+    "stroke-dasharray": longueurChemin + " " + longueurChemin,
+    "stroke-dashoffset": longueurChemin
+});
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
-import { goesToSection, recalcOnResize } from "./functions.js";
-gsap.registerPlugin(MotionPathPlugin);
+import { goesToSection, recalcOnResize, menuAnimation } from "./functions.js";
 
 // Ecouter les scroll (touch) vers le haut
 window.addEventListener('scroll', function(e) {
@@ -15,80 +14,19 @@ window.addEventListener('resize', function(e) {
 
 window.addEventListener('click', function(e){
     if (e.target.id == 'needleNav' || e.target.id == "svgCalque" || e.target.id == "needleCross") {        
-        svgCalque.style.display = "initial";
-        if (needleNav.classList.contains('needleToVertical')) {
-            needleNav.classList.remove('needleToVertical')
-            needleNav.classList.add('needleToHorizontal');
-            monChemin.animate({
-                "stroke-dashoffset": longueurChemin
-            }, 500);
-            gsap.to("#rect", {
-                motionPath: {
-                    path: "#path",
-                    align: "#path",
-                    autoRotate: true,
-                    alignOrigin: [0, 0],
-                    yoyo:true,
-                    curviness:2,
-                    start: 1,
-                    end: 0
-                },
-                transformOrigin: "80% 0%",
-                duration: 0.5,
-            });
-            countMenu = true;
-            setTimeout(function() {
-                svgCalque.style.opacity = "0";
-            }, 500);
-            needleCross.classList.remove('opacityPlus');
-            needleCross.classList.add('opacityLess');
-            menuList.classList.remove('increaseHeight')
-            menuList.classList.add('decreaseHeight')
+        if (collapse) {
+            collapse = false;
         } else {
-            // Pas compris le décalage, mais besoin de décaler à partir du 2e trigger
-            let translateX = countMenu ? 2 : 0;
-            needleNav.classList.add('needleToVertical')
-            needleNav.classList.remove('needleToHorizontal');
-            monChemin.animate({
-                "stroke-dashoffset": 1
-            }, 500);
-            gsap.to("#rect", {
-                motionPath: {
-                    path: "#path",
-                    align: "#path",
-                    autoRotate: true,
-                    alignOrigin: [translateX, 0],
-                    yoyo:true,
-                    curviness:2,
-                },
-                transformOrigin: "80% 0%",
-                duration: 0.5,
-            });
-            svgCalque.style.opacity = "100"
-            needleCross.classList.add('opacityPlus');
-            needleCross.classList.remove('opacityLess');
-            menuList.classList.add('increaseHeight')
-            menuList.classList.remove('decreaseHeight')
+            collapse = true;
         }
+        menuAnimation(collapse)
     }
 })
 
 AOS.init();
 
-var svg = Snap("#svgCalque");
-var monChemin = svg.select("#path");
-
-// Longueur totale du chemin du "path"
-var longueurChemin = Snap.path.getTotalLength(monChemin);
-
-// Définir la longueur du chemin à 0 au début de l'animation
-monChemin.attr({
-    "stroke-dasharray": longueurChemin + " " + longueurChemin,
-    "stroke-dashoffset": longueurChemin
-});
-
-let countMenu = false;
-
 document.addEventListener('DOMContentLoaded', function() {
     recalcOnResize();
 });
+
+let collapse = true;
